1654a6674203704cadde132907934c901b8d8cac
Alicia Gonzalez Castro <aliglezcastro@gmail.com>
2023-12-19 10:08:03 +0100

    first commit

---
+++ src/main/java/ali/glez/apimovie/ApimovieApplication.java
@@ -0,0 +1,13 @@
+package ali.glez.apimovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ali/glez/apimovie/ApimovieApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.glez.apimovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
