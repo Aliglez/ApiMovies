aadad38add9a65a0b9d2d6cea45d14d41dd1af97
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-26 11:06:35 +0100

    created Country Model

---
+++ src/main/java/dev/ali/ApiMovies/models/Country.java
@@ -0,0 +1,50 @@
+package dev.ali.ApiMovies.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    private String name;
+
+
+    public Country() {
+    }
+
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
