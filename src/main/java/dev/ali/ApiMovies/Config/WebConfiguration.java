ff9276c78750b0c0f9f9d26662fa3bfd536f90b7
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-27 18:58:36 +0100

    added addCorsMappings

---
+++ src/main/java/dev/ali/ApiMovies/Config/WebConfiguration.java
@@ -0,0 +1,21 @@
+package dev.ali.ApiMovies.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("PUT", "DELETE", "PUT", "DELETE")
+			.allowCredentials(false);
+    }
+    
+    
+}
