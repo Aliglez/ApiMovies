9a612a6290106ae372cc8493191709b0481eca72
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-29 07:32:38 +0100

    created movies model

---
+++ src/main/java/dev/ali/ApiMovies/models/Movies.java
@@ -0,0 +1,75 @@
+package dev.ali.ApiMovies.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class Movies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_movie")
+    private Long id;
+    
+    private String title;
+    private short year;
+    private String description;
+    private short running_time;
+
+    public Movies() {
+    }
+
+    public Movies(String title, short year, String description, short running_time) {
+        this.title = title;
+        this.year = year;
+        this.description = description;
+        this.running_time = running_time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public short getRunning_time() {
+        return running_time;
+    }
+
+    public void setRunning_time(short running_time) {
+        this.running_time = running_time;
+    }
+
+    
+    
+}
